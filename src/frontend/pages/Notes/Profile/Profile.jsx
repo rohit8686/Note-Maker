100af27f1a7a99d7ca87b5cf6a3fe3d4b1f04d25
gundarohit <88605957+rohit8686@users.noreply.github.com>
2022-04-05 11:49:25 +0530

    feat : profile page

---
+++ src/frontend/pages/Notes/Profile/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useAuth } from "../../../contexts/auth-context";
+
+export const Profile = () => {
+  const {
+    authState: { userData },
+    logout,
+  } = useAuth();
+  console.log(userData);
+
+  return (
+    <div>
+      <h2 className="flex py-1">
+        Hey, {userData.firstName} {userData.lastName}
+      </h2>
+      <button className="btn flex mx-auto" onClick={logout}>
+        Logout
+      </button>
+    </div>
+  );
+};
