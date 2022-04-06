4532f9adc2a95036b5c4284b3398eb4bf49b7356
gundarohit <88605957+rohit8686@users.noreply.github.com>
2022-04-06 16:17:45 +0530

    fix : routing

--- src/frontend/routes/Routing.jsx
+++ src/frontend/routes/Routing.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Home } from "../../frontend/pages/Home/Home";
-import { Login } from "../../frontend/pages/Login/Login";
-import { NoteHome } from "../../frontend/pages/Notes/NoteHome/NoteHome";
-import { NotFound } from "../../frontend/pages/NotFound/NotFound";
-import { Signup } from "../../frontend/pages/Signup/Signup";
+import { Home } from "../pages/Home/Home";
+import { Login } from "../pages/Login/Login";
+import { NoteHome } from "../pages/Notes/NoteHome/NoteHome";
+import { NotFound } from "../pages/NotFound/NotFound";
+import { Signup } from "../pages/Signup/Signup";
 
 export const Routing = () => {
   return (
